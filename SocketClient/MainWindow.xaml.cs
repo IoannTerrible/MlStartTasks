74bf39a10570db8cc32ebd887e2cb48404b790c7
IoannTerrible <116111680+IoannTerrible@users.noreply.github.com>
2024-03-05 20:51:00 +0300

    0.99.3.001 Start to create socket app

---
+++ SocketClient/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SocketClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StoryClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DisconnectClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetIpButton_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- WpfApp1/obj/Release/net7.0-windows/WpfApp1.AssemblyInfo.cs
+++ WpfApp1/obj/Release/net7.0-windows/WpfApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406ddc05f8d22ac3a036965ce95d638bb83ec7a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ed31aa664ddd400467d5515cea36dfcb321777")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
