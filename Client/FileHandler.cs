a4bd397f97e68688b2298c9eef7bd87aee35b866
SSD4TB <cbro82277@gmail.com>
2024-04-06 11:30:31 +0300

    1.016 added filehandler, small code ref, mod videopage

---
+++ Client/FileHandler.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary;
+using Microsoft.Win32;
+using Serilog.Events;
+
+namespace Client
+{
+    public class FileHandler
+    {
+        private static Dictionary<string, string> fileTypes = new()
+        {
+            { "Image", "Image files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp" },
+            { "Media", "Media files (*.mp4;*.avi;*.wmv)|*.mp4;*.avi;*.wmv" }
+        };
+        public static string? OpenFile(string fileType)
+        {
+            OpenFileDialog ofd = new()
+            {
+                Multiselect = false,
+                Filter = fileTypes[fileType]
+            };
+            if(ofd.ShowDialog() == true)
+            {
+                string filename = ofd.FileName;
+                string shortFileName = Logger.GetLastFile(filename);
+                if (!System.IO.File.Exists(filename))
+                {
+                    Logger.LogByTemplate(LogEventLevel.Error, note: "Selected file does not exist: " + shortFileName);
+                    return null;
+                }
+                return filename;
+            }
+            else
+            {
+                Logger.LogByTemplate(LogEventLevel.Warning, note: "No file selected.");
+                return null;
+            }
+        }
+    }
+}
--- Client/Pages/VideoPage.xaml.cs
+++ Client/Pages/VideoPage.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using ClassLibrary;
-using Microsoft.Win32;
+using Client;
 using Serilog.Events;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace SocketClient
 {
@@ -12,44 +12,67 @@ namespace SocketClient
     /// </summary>
     public partial class VideoPage : Page
     {
+        private DispatcherTimer timer = new ();
         public VideoPage()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += timer_tick;
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            time.Text = mediaElement1.Position.ToString(@"mm\:ss");
+            sliderback2.Value = mediaElement1.Position.TotalSeconds;
         }
 
         private void MediaPlayButton_Click(object sender, RoutedEventArgs e)
         {
             mediaElement1.Play();
+            timer.Start();
+        }
+
+        private void MediaPauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Pause();
+            timer.Stop();
         }
 
         private void MediaStopButton_Click(object sender, RoutedEventArgs e)
         {
             mediaElement1.Stop();
+            timer.Stop();
+        }
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaElement1.Pause();
+            mediaElement1.Position = TimeSpan.FromSeconds(slider2.Value);
+            mediaElement1.Play();
+            timer.Start();
+        }
+        private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            slider2.Maximum = mediaElement1.NaturalDuration.TimeSpan.TotalSeconds;
+            sliderback2.Maximum = mediaElement1.NaturalDuration.TimeSpan.TotalSeconds;
         }
 
         private void UploadMediaButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new()
+            try
             {
-                Multiselect = false,
-                Filter = "Media files (*.mp4;*.avi)|*.mp4;*.avi"
-            };
+                string? filename = FileHandler.OpenFile("Media");
 
-            if (openFileDialog.ShowDialog() == true)
-            {
-                string filename = openFileDialog.FileName;
-                string shortFileName = Logger.GetLastFile(filename);
-                if (!System.IO.File.Exists(filename))
+                if (filename != null)
                 {
-                    Logger.LogByTemplate(LogEventLevel.Error, note: "Selected file does not exist: " + shortFileName);
-                    return;
+                    mediaElement1.Source = new Uri(filename);
+                    Logger.LogByTemplate(LogEventLevel.Information, note: $"Media file opened from {filename}");
+                    MediaPlayButton_Click(sender, e);
                 }
-                mediaElement1.Source = new Uri(filename);
-                Logger.LogByTemplate(LogEventLevel.Information, note: $"media file opened from {filename}");
             }
-            else
+            catch (Exception ex)
             {
-                Logger.LogByTemplate(LogEventLevel.Warning, note: "No file selected.");
+                Logger.LogByTemplate(LogEventLevel.Error, ex, note: "Media file openning error.");
+                MessageBox.Show($"An unexpected error occurred: {ex.Message}");
             }
         }
     }
